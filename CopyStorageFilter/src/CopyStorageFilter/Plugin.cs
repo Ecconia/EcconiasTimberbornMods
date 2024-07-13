a925097ae01e1108bfc1893aaa3e143a11f564dd
Ecconia <public-commit@ecconia.com>
2024-07-13 22:16:50 +0200

    CSF: Make compatible with game Update 6 exp
    Switches to the new mod loader endpoint. Loads services via a configurator. Removes BepInEx references. Adds manifest file.

--- CopyStorageFilter/src/CopyStorageFilter/Plugin.cs
+++ CopyStorageFilter/src/CopyStorageFilter/Plugin.cs
@@ -1,30 +1,26 @@
-using System.Reflection;
-using BepInEx;
 using Bindito.Core;
 using HarmonyLib;
-using Timberborn.SelectionSystem;
+using Timberborn.ModManagerScene;
 
 namespace CopyStorageFilter
 {
-	[BepInPlugin("ecconia.timberborn.copystoragefilter", "Copy Storage Filter", "2.0.1")]
-	public class Plugin : BaseUnityPlugin
+	public class Plugin : IModStarter
 	{
-		private void Awake()
+		public void StartMod()
 		{
 			var harmony = new Harmony("ecconia.timberborn.copystoragefilter");
-			
-			//Hook a service (for easy access into the dependency system):
-			var stuff = typeof(SelectionSystemConfigurator).GetMethod("Configure", BindingFlags.Public | BindingFlags.Instance);
-			var hook = typeof(Plugin).GetMethod(nameof(serviceHook), BindingFlags.Public | BindingFlags.Static);
-			harmony.Patch(stuff, null, new HarmonyMethod(hook));
 			
 			//Setup all the reflection/harmony stuff for the CursorTool hooks: 
 			CursorToolHook.init(harmony);
 		}
 		
-		public static void serviceHook(IContainerDefinition containerDefinition)
+		[Context("Game")]
+		public class CopyStorageFilterConfigurator : IConfigurator
 		{
-			containerDefinition.Bind<CopyTool>().AsSingleton();
+			public void Configure(IContainerDefinition containerDefinition)
+			{
+				containerDefinition.Bind<CopyTool>().AsSingleton();
+			}
 		}
 	}
 }
